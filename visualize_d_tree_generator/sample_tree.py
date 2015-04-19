17a5dc74509002d80e534c0aeb8bbac517da6af4
Dan Silver <dannysilver3@gmail.com>
2015-04-19 11:08:06 -0500

    sample update

--- visualize_d_tree_generator/sample_tree.py
+++ visualize_d_tree_generator/sample_tree.py
@@ -4,13 +4,14 @@ from convert_tree_to_json import ConvertTreeToJSON
 
 data, labels = dummy_data.generate(20*1000, ['Age', 'Weight', 'Hair Color', 'Birth City', 'Current City'])
 
-# create decision tree
+# Create a decision tree - http://scikit-learn.org/stable/modules/tree.html#classification
 dt = DecisionTreeClassifier()
 dt.fit(data[0], data[1])
 
-json_generator = ConvertTreeToJSON(dt, labels)
-json = json_generator.convert()
+# Generate JSON that represents the tree's structure
+json = ConvertTreeToJSON(dt, labels).convert()
 
+# Save the json to a file
 text_file = open("data.json", "w")
 text_file.write(json)
 text_file.close()
